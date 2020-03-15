eefdd8f05153768fd444879198c3560e333fdbf4
Ratoone <vacariu.paul@gmail.com>
2020-03-15 20:21:36 +0100

    Added UI for parameter calibration

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 import logging
+import sys
 
 import cv2
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QSlider
+from PyQt5 import uic
 
-from preprocessing.Preprocessing import Preprocessing
+from ImageProcessing import Preprocessing
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi("pipeline.ui", self)
+
+        self.show()
+
+        self.compute_disparity = self.findChild(QPushButton, "compute_disparity")
+        self.compute_disparity.clicked.connect(lambda: print("shit"))
+
+        self.image_placeholder = self.findChild(QLabel, "disparity_image")
+
+        self.block_size_slider = self.findChild(QSlider, "block_size")
+        self.min_disparity_slider = self.findChild(QSlider, "min_disparity")
+        self.num_disparity_slider = self.findChild(QSlider, "num_disparity")
+        self.p1_slider = self.findChild(QSlider, "p1")
+        self.p2_slider = self.findChild(QSlider, "p2")
+        self.max_dif_slider = self.findChild(QSlider, "disp_max_dif")
+        self.uniqueness_slider = self.findChild(QSlider, "uniqueness")
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - [%(name)s] %(levelname)s: %(message)s")
     preprocessing = Preprocessing()
 
-    img1 = cv2.imread("./images/Calibratie 1/calibrationLeft/Calibratie 1_L_137.jpg")
-    img2 = cv2.imread("./images/Calibratie 1/calibrationMiddle/Calibratie 1_M_137.jpg")
-    img3 = cv2.imread("./images/Calibratie 1/calibrationRight/Calibratie 1_R_137.jpg")
+    img1 = cv2.imread("./images/subject1/subject1Left/subject1_Left_1.jpg")
+    img2 = cv2.imread("./images/subject1/subject1Middle/subject1_Middle_1.jpg")
+    img3 = cv2.imread("./images/subject1/subject1Right/subject1_Right_1.jpg")
+
+    # img1 = cv2.imread("./images/Calibratie 1/calibrationLeft/Calibratie 1_L_1.jpg")
+    # img2 = cv2.imread("./images/Calibratie 1/calibrationMiddle/Calibratie 1_M_1.jpg")
+    # img3 = cv2.imread("./images/subject1/subject1Right/subject1_Right_1.jpg")
 
     preprocessing.preprocess_image_batch(img1, img2, img3)
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - [%(name)s] %(levelname)s: %(message)s")
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+
+    app.exec_()
